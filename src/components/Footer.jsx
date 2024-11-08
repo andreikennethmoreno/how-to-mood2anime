25640223b80932abeff763a953ff0757395cdf4d
MakeArtKenji <Kennonirom@gmail.com>
2024-11-08 21:55:24 +0800

    deployment time

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="footer footer-center bg-base-100 text-base-content p-4">
+            <aside>
+                <p>Made by &nbsp;
+                    <a target="_blank" href="https://mood2-anime.vercel.app/" className="text-primary underline">Kenn Onirom</a>
+                </p>
+            </aside>
+            </footer>
+        </>
+    )
+}
